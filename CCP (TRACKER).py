27d4832939ed043d7701fe56a688b4b598a3349a
“Ambrish” <10d.ambrish.s.2376@gmail.com>
2025-10-01 10:50:05 +0530

    Initial commit of Concentration Tracker Project

---
+++ CCP (TRACKER).py
@@ -0,0 +1,6 @@
+ #    cd ~/Downloads/concentration_tracker-main
+#     python3.11 -m venv venv
+# source venv/bin/activate
+# pip install mediapipe opencv-python
+#pip install -r requirements.txt
+#python3 concen_tracker.py
